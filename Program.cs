6595c50d90bee8c1d2f3052b711447418b32e154
Quincy Miller <quincy.m.miller@outlook.com>
2024-12-20 09:06:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment7._3._1
+{
+    //1. You are given the root of a binary search tree (BST) and an integer val.
+    //Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constructing the binary search tree
+            TreeNode root = new TreeNode(4,
+                             new TreeNode(2,
+                                 new TreeNode(1),
+                                 new TreeNode(3)),
+                             new TreeNode(7));
+
+            Solution solution = new Solution();
+            TreeNode result = solution.SearchBST(root, 2);
+
+            if (result != null)
+            {
+                Console.WriteLine($"Found subtree with root value: {result.val}");
+            }
+            else
+            {
+                Console.WriteLine("Value not found in the tree.");
+            }
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7._3._1
+{
+    //solution class containing the SearchBST method
+    public class Solution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null || root.val == val)
+            {
+                return root;
+            }
+
+            //search the left or right subtree based on the BST property
+            if (val < root.val)
+            {
+                return SearchBST(root.left, val);
+            }
+            else
+            {
+                return SearchBST(root.right, val);
+            }
+        }
+    }
+}
